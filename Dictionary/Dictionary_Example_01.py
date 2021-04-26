10ee5c68d15b59e9e3549893c04bc70e2ba4d304
moses <academicsnotes@gmail.com>
2021-04-26 22:57:19 +0800

    Add script

--- Dictionary/Dictionary_Example_01.py
+++ Dictionary/Dictionary_Example_01.py
@@ -10,3 +10,20 @@ print(ages["John"])
 
 ages.pop("John") # Remove a key value pair from dictionary
 
+"""
+    Dictionaries: Looping through keys
+"""
+customer_29876 = {
+    "first name": "David",
+    "last name": "Elliott",
+    "address": "4803 Wellesley St."
+}
+
+for each_key in customer_29876.keys():
+    print(each_key)
+
+for each_value in customer_29876.values():
+    print(each_value)
+
+for each_key, each_value in customer_29876.items():
+    print("The customer's " + each_key + " is " + each_value)
