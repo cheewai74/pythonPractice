ef17edcdafc111e9fe8579786fb832ab1fc71752
moses <academicsnotes@gmail.com>
2021-06-06 13:44:13 +0800

    Add script

--- tkinter/helloWorld.py
+++ tkinter/helloWorld.py
@@ -1,12 +1,10 @@
-import tkinter
 from tkinter import *
-##Import of Tkinter module
 
 window = Tk()
-##Creates the window from the imported Tkinter module
+# Creates the window from the imported Tkinter module
 window.geometry("600x400")
-##Creates the size of the window
+# Creates the size of the window
 window.title("Test :)")
-##Adds a title to the Windows GUI for the window
+# Adds a title to the Windows GUI for the window
 
 window.mainloop()
