89486fb790c6a3589152e91dc766df31b4a41a10
moses <academicsnotes@gmail.com>
2021-08-12 13:57:43 +0800

    Add script

--- Sample_Programs/adding.py
+++ Sample_Programs/adding.py
@@ -1,17 +1,23 @@
 def main():
     amt = 0
     while True:
-        b, n = get_num()
-        if not b:
+        x = get_num()
+        if x is None:
+        #b, n = get_num()
+        #if not b:
             break
-        amt += n
+        #amt += n
+        amt += x
     print('The total is :', amt)
 
-def get_num(num = 0.0):
+#def get_num(num = 0.0):
+def get_num():
     s = input('Input number (ENTER to quit): ')
     if not s:
-        return False, num
+        # return False, num
+        return None
     else:
-        return True, float(s)
+        #return True, float(s)
+        return float(s)
 
 main()
